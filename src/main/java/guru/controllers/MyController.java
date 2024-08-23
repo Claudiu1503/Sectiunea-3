b4f673d1f1b0474dadc45fe8782282a907808da4
Claudiu_1503 <adichis1503@yahoo.com>
2024-08-23 14:20:22 +0300

    Empty Spring Project - First Push

---
+++ src/main/java/guru/controllers/MyController.java
@@ -0,0 +1,4 @@
+package guru.controllers;
+
+public class MyController {
+}
---
+++ src/test/java/springframework/sec3/Sec3ApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.sec3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
